12e3b1b417ccb81a6aa5f04904ec77bc87510d5c
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-08-04 02:38:07 +0700

    insert tag form to add/edit form

--- src/App.js
+++ src/App.js
@@ -13,18 +13,20 @@ function App() {
           <div className="d-flex justify-content-end">
             <button className="btn-close btn-sm text-black-50"></button>
           </div>
-          <div className="mt-3 mb-4">
-            <input
-              type="text"
-              className="form-control form-control-lg is-invalid"
-              id="todo"
-              placeholder="Enter new todo"
-            />
-            <div className="invalid-feedback">Required</div>
-          </div>
-          <button className="btn btn-secondary">
-            <i className="bi-save" />
-          </button>
+          <form>
+            <div className="mt-3 mb-4">
+              <input
+                type="text"
+                className="form-control form-control-lg is-invalid"
+                id="todo"
+                placeholder="Enter new todo"
+              />
+              <div className="invalid-feedback">Required</div>
+            </div>
+            <button className="btn btn-secondary">
+              <i className="bi-save" />
+            </button>
+          </form>
         </div>
         <div className="card shadow rounded-0 mb-3 bd-callout bd-callout-success">
           <div className="card-body d-flex justify-content-between align-items-center">
