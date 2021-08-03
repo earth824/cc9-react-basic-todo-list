60d2835ac368697fadc9c926d9177ee51d7f57d8
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-08-04 02:33:08 +0700

    finish html css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mw-576 my-4 mx-auto">
+        <div className="mb-4 d-flex justify-content-end">
+          <button className="btn btn-success">
+            <i className="bi-plus-lg" />
+          </button>
+        </div>
+        <div className="border shadow p-3 mb-4">
+          <div className="d-flex justify-content-end">
+            <button className="btn-close btn-sm text-black-50"></button>
+          </div>
+          <div className="mt-3 mb-4">
+            <input
+              type="text"
+              className="form-control form-control-lg is-invalid"
+              id="todo"
+              placeholder="Enter new todo"
+            />
+            <div className="invalid-feedback">Required</div>
+          </div>
+          <button className="btn btn-secondary">
+            <i className="bi-save" />
+          </button>
+        </div>
+        <div className="card shadow rounded-0 mb-3 bd-callout bd-callout-success">
+          <div className="card-body d-flex justify-content-between align-items-center">
+            <h5 className="mb-0">Task 1</h5>
+            <div className="btn-group">
+              <button className="btn btn-outline-info">
+                <i className="bi-toggle2-on" />
+              </button>
+              <button className="btn btn-primary">
+                <i className="bi-pencil-square" />
+              </button>
+              <button className="btn btn-danger">
+                <i className="bi-trash" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card shadow rounded-0 mb-3 bd-callout bd-callout-warning">
+          <div className="card-body d-flex justify-content-between align-items-center">
+            <h5 className="mb-0">Task 1</h5>
+            <div className="btn-group">
+              <button className="btn btn-outline-info">
+                <i className="bi-toggle2-off" />
+              </button>
+              <button className="btn btn-primary">
+                <i className="bi-pencil-square" />
+              </button>
+              <button className="btn btn-danger">
+                <i className="bi-trash" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
