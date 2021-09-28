974b8da2af3d64397e757fb56e351654188bfaa5
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-09-28 11:12:27 +0700

    add login form

--- src/App.js
+++ src/App.js
@@ -2,83 +2,137 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <div className="mw-576 my-4 mx-auto">
-        <div className="mb-4 d-flex justify-content-end">
-          <button className="btn btn-success">
-            <i className="bi-plus-lg" />
-          </button>
-        </div>
-        <div className="border shadow p-3 mb-4">
-          <div className="d-flex justify-content-end">
-            <button className="btn-close btn-sm text-black-50"></button>
+    <>
+      <nav className="navbar navbar-expand-sm sticky-top navbar-dark bg-success">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand text-black-50 fw-bolder ts-8">
+            TODO LIST APP
+          </a>
+          <div className="navbar-collapse justify-content-end">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Logout
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Register
+                </a>
+              </li>
+            </ul>
           </div>
-          <form>
-            <div className="mt-3 mb-4">
-              <input
-                type="text"
-                className="form-control form-control-lg is-invalid"
-                id="todo"
-                placeholder="Enter new todo"
-              />
-              <div className="invalid-feedback">Required</div>
-            </div>
-            <button className="btn btn-secondary">
-              <i className="bi-save" />
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="mw-576 my-4 mx-auto">
+          <div class="alert alert-danger">Error Message</div>
+          <div className="border shadow p-3 mb-4">
+            <form>
+              <div className="mb-3">
+                <label for="" className="form-label">
+                  Email address
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="mb-4">
+                <label for="" className="form-label">
+                  Password
+                </label>
+                <input type="password" className="form-control" />
+              </div>
+              <button type="submit" className="btn btn-success">
+                Login
+              </button>
+            </form>
+          </div>
+
+          {/* <div className="mb-4 d-flex justify-content-end">
+            <button className="btn btn-success">
+              <i className="bi-plus-lg" />
             </button>
-          </form>
-        </div>
-        <div className="alert alert-danger text-center">1 Task Remaining</div>
-        <div className="card shadow rounded-0 bd-callout bd-callout-success">
-          <div className="card-body d-flex justify-content-between align-items-center">
-            <span>Task 3</span>
-            <div className="btn-group">
-              <button className="btn btn-outline-info">
-                <i className="bi-toggle2-on" />
+          </div> */}
+          <div className="border shadow p-3 mb-4">
+            {/* <div className="d-flex justify-content-end">
+              <button className="btn-close btn-sm text-black-50"></button>
+            </div> */}
+            <form>
+              <div className="mt-3 mb-4">
+                <input type="text" className="form-control is-invalid" id="todo" placeholder="Enter new todo" />
+                <div className="invalid-feedback">Required</div>
+              </div>
+              <button className="btn btn-secondary">
+                <i className="bi-save" />
               </button>
-              <button className="btn btn-primary">
-                <i className="bi-pencil-square" />
-              </button>
-              <button className="btn btn-danger">
-                <i className="bi-trash" />
-              </button>
+            </form>
+          </div>
+          <div className="card shadow rounded-0">
+            <div className="card-body d-flex justify-content-center bg-warning">
+              <span className="text-black-50">1 Task Remaining</span>
             </div>
           </div>
-        </div>
-        <div className="card shadow rounded-0 bd-callout bd-callout-success">
-          <div className="card-body d-flex justify-content-between align-items-center">
-            <span>Task 2</span>
-            <div className="btn-group">
-              <button className="btn btn-outline-info">
-                <i className="bi-toggle2-on" />
-              </button>
-              <button className="btn btn-primary">
-                <i className="bi-pencil-square" />
-              </button>
-              <button className="btn btn-danger">
-                <i className="bi-trash" />
-              </button>
+          <div className="card shadow rounded-0 bd-callout bd-callout-success">
+            <div className="card-body d-flex justify-content-between align-items-center">
+              <span>Task 3</span>
+              <div className="btn-group">
+                <button className="btn btn-outline-info">
+                  <i className="bi-toggle2-on" />
+                </button>
+                <button className="btn btn-primary">
+                  <i className="bi-pencil-square" />
+                </button>
+                <button className="btn btn-danger">
+                  <i className="bi-trash" />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card shadow rounded-0 mb-3 bd-callout bd-callout-warning">
-          <div className="card-body d-flex justify-content-between align-items-center">
-            <span>Task 1</span>
-            <div className="btn-group">
-              <button className="btn btn-outline-info">
-                <i className="bi-toggle2-off" />
-              </button>
-              <button className="btn btn-primary">
-                <i className="bi-pencil-square" />
-              </button>
-              <button className="btn btn-danger">
-                <i className="bi-trash" />
-              </button>
+          <div className="card shadow rounded-0 bd-callout bd-callout-success">
+            <div className="card-body d-flex justify-content-between align-items-center">
+              <span>Task 2</span>
+              <div className="btn-group">
+                <button className="btn btn-outline-info">
+                  <i className="bi-toggle2-on" />
+                </button>
+                <button className="btn btn-primary">
+                  <i className="bi-pencil-square" />
+                </button>
+                <button className="btn btn-danger">
+                  <i className="bi-trash" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card shadow rounded-0 mb-3 bd-callout bd-callout-warning">
+            <div className="card-body d-flex justify-content-between align-items-center">
+              <span>Task 1</span>
+              <div className="btn-group">
+                <button className="btn btn-outline-info">
+                  <i className="bi-toggle2-off" />
+                </button>
+                <button className="btn btn-primary">
+                  <i className="bi-pencil-square" />
+                </button>
+                <button className="btn btn-danger">
+                  <i className="bi-trash" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
